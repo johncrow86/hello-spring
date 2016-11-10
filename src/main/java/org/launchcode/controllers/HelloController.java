734b869b71a4f4cbc0322157b6f6ea54b01ed1d2
John Crow <johncrow86@hotmail.com>
2016-11-10 17:36:23 -0600

    doesnt auto-create tables

--- src/main/java/org/launchcode/controllers/HelloController.java
+++ src/main/java/org/launchcode/controllers/HelloController.java
@@ -1,8 +1,13 @@
 package org.launchcode.controllers;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.launchcode.models.HelloLog;
 import org.launchcode.models.HelloMessage;
+import org.launchcode.models.dao.HelloLogDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +16,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
+	
+	@Autowired
+	private HelloLogDao helloLogDao;
 	
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	public String helloForm() {
@@ -27,8 +35,20 @@ public class HelloController {
 		if (name == null || name == "")
 			name = "World";
 		
+		HelloLog helloLog = new HelloLog(name);
+		helloLogDao.save(helloLog);
+		
 		model.addAttribute("message", HelloMessage.getMessage(name, language));
 		model.addAttribute("title", "Hello Spring! Response");
 		return "hello";
+	}
+	
+	@RequestMapping(value = "/log")
+	public String log(Model model) {
+		
+		List<HelloLog> logs = helloLogDao.findAll();
+		model.addAttribute("logs", logs);
+		
+		return "log";
 	}
 }
